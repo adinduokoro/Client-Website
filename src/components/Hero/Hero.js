4088e0acc86d57be16f431882ba6fc4efef619d2
Addie Okoro <adinduokoro95@gmail.com>
2023-05-31 16:41:02 -0400

    Hero
    The hero section is mostly complete and structured.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import "./Hero.css"
+import React from "react";
+import "./Hero.css";
+import heroPic from "../../images/hero-pic.jpg";
+import logo from "../../images/logo.png";
+
 
 const Hero = () => {
   return (
-    <section className='hero'>
-      hello
+    <section className="hero">
+      <div className="hero__img">
+        <img src={heroPic} alt="hero__img" />
+      </div>
+      <div className="hero__overlay"></div>
+      <div className="hero__info">
+        <img src={logo} alt="" />
+        <h1>WASHINGTON'S DIRECT</h1>
+        <h2>FIREARMS TRAINING</h2>
+        <h3>Qualified Instructors Teaching Safety and Defense Skills</h3>
+        <a href="" className="hero__button button">
+          Book Now
+        </a>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
