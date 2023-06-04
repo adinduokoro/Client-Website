e95cf5b3bd9129fddfd5e2fc748c6f8af9fa502a
Addie Okoro <adinduokoro95@gmail.com>
2023-06-03 20:19:00 -0400

    Offer
    Creating the structure of the car section.

--- src/components/Offer/Offer.js
+++ src/components/Offer/Offer.js
@@ -1,31 +1,25 @@
-import React from 'react'
-import "./Offer.css"
+import React from "react";
+import "./Offer.css";
+import { services } from "./data";
 
 const Offer = () => {
-  const service = [
-    {text: "FIRST TIME SHOOTER"},
-    {text: "CLASSES & TRAINING"},
-    {text: "SELF DEFENSE TRAINING"},
-    {text: "INITIAL WEAR & CARRY/RENEWAL"},
-    {text: "SECURITY GUARD CLEARANCE"},
-    {text: "HANDGUN QUALIFICATION CERT. (HQL)"},
-  ]
-
   return (
-    <section className='offer' id='offer' style={{ scrollMarginTop: "7.5rem" }}>
+    <section className="offer" id="offer" style={{ scrollMarginTop: "7.5rem" }}>
       <div className="offer__container container">
         <h2 className="section__title">What We Offer</h2>
         <span className="section__subtitle">Firearm Saftey & Training</span>
         <div className="offerCard__container">
-          <div className="offerCard">
-            First Time Shooter
-          </div>
+          {services.map((service, index) => {
+            return (
+              <div className="offerCard" key={index}>
+                {service.text}
+              </div>
+            );
+          })}
         </div>
-
-
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Offer
+export default Offer;
---
+++ src/components/Offer/data.js
@@ -0,0 +1,10 @@
+export const services = [
+  { text: "FIRST TIME SHOOTER" },
+  { text: "CLASSES & TRAINING" },
+  { text: "SELF DEFENSE TRAINING" },
+  { text: "INITIAL WEAR & CARRY/RENEWAL" },
+  { text: "SECURITY GUARD CLEARANCE" },
+  { text: "HANDGUN QUALIFICATION CERT. (HQL)" },
+];
+
+export default { services };
