d7f3a83db76a79c11cc286c49604248b171ca584
Addie Okoro <adinduokoro95@gmail.com>
2023-06-05 16:59:47 -0400

    Offer
    The offer section is completely functional and styled. Working on the pricing section.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,29 +5,42 @@ import Offer from "../../components/Offer/Offer";
 import "./Home.css";
 import { services } from "../../components/Offer/data";
 import popupImg from "../../assets/images/popup-bg.jpg";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { Icon } from "@iconify/react";
+import { showPopup } from "../../features/popup/popupSlice";
 
 const Home = () => {
-  const show = useSelector((store) => store.popup.show)
+  const show = useSelector((store) => store.popup.show);
+  const index = useSelector((store) => store.popup.value);
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(showPopup(false));
+  };
 
   return (
     <div className="home">
       <>
-        <div className={show ? "home__overlay show" : "home__overlay"}></div>
-        <div className="popup__container">
+        <div
+          className={show ? "home__overlay show" : "home__overlay"}
+          onClick={handleClick}
+        ></div>
+        <div className={show ? "popup__container show" : "popup__container"}>
           <div className="popup__left">
             <img src={popupImg} alt="" />
             <div className="popup__course">
               <h2 style={{ letterSpacing: "2px" }}>COURSE:</h2>
-              <p>{services[0].text}</p>
+              <p>{services[index].text}</p>
             </div>
           </div>
           <div className="popup__right">
             <div className="popup__rightText">
               <h2 style={{ letterSpacing: "2px" }}>DESCRIPTION:</h2>
-              <p>{services[0].description}</p>
+              <p>{services[index].description}</p>
             </div>
-            <div className="popup__button">X</div>
+            <div className="popup__button">
+              <Icon icon="mingcute:close-fill" onClick={handleClick} />
+            </div>
           </div>
         </div>
       </>
