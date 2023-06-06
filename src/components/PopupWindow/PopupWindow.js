f991f7409b513c4ba472b3e67eeddaad6dd00828
Addie Okoro <adinduokoro95@gmail.com>
2023-06-05 22:58:48 -0400

    Popup Window
    Update

---
+++ src/components/PopupWindow/PopupWindow.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./PopupWindow.css";
+import { useSelector, useDispatch } from "react-redux";
+import popupImg from "../../assets/images/popup-bg.jpg";
+import { services } from "../../components/Offer/data";
+import { Icon } from "@iconify/react";
+import { showPopup } from "../../features/popup/popupSlice";
+
+
+
+
+
+
+
+const PopupWindow = () => {
+  const show = useSelector((store) => store.popup.show);
+  const index = useSelector((store) => store.popup.value);
+  const dispatch = useDispatch();
+
+
+  const handleClick = () => {
+    dispatch(showPopup(false));
+  };
+
+  return (
+    <div className={show ? "popup__container show" : "popup__container"}>
+      <div className="popup__left">
+        <img src={popupImg} alt="" />
+        <div className="popup__course">
+          <h2 style={{ letterSpacing: "2px" }}>COURSE:</h2>
+          <p>{services[index].text}</p>
+        </div>
+      </div>
+      <div className="popup__right">
+        <div className="popup__rightText">
+          <h2 style={{ letterSpacing: "2px" }}>DESCRIPTION:</h2>
+
+          <p>{services[index].description}</p>
+        </div>
+        <div className="popup__button">
+          <Icon icon="mingcute:close-fill" onClick={handleClick} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopupWindow;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,16 +2,13 @@ import React from "react";
 import Hero from "../../components/Hero/Hero";
 import About from "../../components/About/About";
 import Offer from "../../components/Offer/Offer";
+import PopupWindow from "../../components/PopupWindow/PopupWindow";
 import "./Home.css";
-import { services } from "../../components/Offer/data";
-import popupImg from "../../assets/images/popup-bg.jpg";
 import { useSelector, useDispatch } from "react-redux";
-import { Icon } from "@iconify/react";
 import { showPopup } from "../../features/popup/popupSlice";
 
 const Home = () => {
   const show = useSelector((store) => store.popup.show);
-  const index = useSelector((store) => store.popup.value);
   const dispatch = useDispatch();
 
   const handleClick = () => {
@@ -25,27 +22,7 @@ const Home = () => {
           className={show ? "home__overlay show" : "home__overlay"}
           onClick={handleClick}
         ></div>
-        <div className={show ? "popup__container show" : "popup__container"}>
-          <div className="popup__left">
-            <img src={popupImg} alt="" />
-            <div className="popup__course">
-              <h2 style={{ letterSpacing: "2px" }}>COURSE:</h2>
-              <br />
-              <p>{services[index].text}</p>
-            </div>
-          </div>
-          <div className="popup__right">
-            <div className="popup__rightText">
-              <h2 style={{ letterSpacing: "2px" }}>DESCRIPTION:</h2>
-              <br />
-
-              <p>{services[index].description}</p>
-            </div>
-            <div className="popup__button">
-              <Icon icon="mingcute:close-fill" onClick={handleClick} />
-            </div>
-          </div>
-        </div>
+        <PopupWindow />
       </>
       <>
         <Hero />
