7af1aaf63ab5c9c87c5b3154e96f21e510356bb2
Addie Okoro <adinduokoro95@gmail.com>
2023-06-06 14:48:59 -0400

    Classes
    Finished structuring half of the course section. Working on the other half, which pricing.

--- src/components/Classes/Classes.js
+++ src/components/Classes/Classes.js
@@ -1,18 +1,27 @@
 import React from "react";
 import "./Classes.css";
+import logo from "../../assets/images/washington-logo.png";
 
 const Classes = () => {
   return (
-    <div className="classes" id="classes" style={{ scrollMarginTop: "7.5rem" }}>
+    <section className="classes" id="classes" style={{ scrollMarginTop: "7.5rem" }}>
       <div className="classes__container container">
         <h2 className="section__title">Classes & Training</h2>
         <span className="section__subtitle">Course Pricing</span>
-        <div className="package__container">
-          <div className="left"></div>
-          <div className="right"></div>
+        <div className="pricing__container">
+          <div className="logo__left">
+            <img src={logo} alt="" />
+            <div className="logo__text">
+              <p className="info__title">FIREARMS</p>
+              <p className="info__subTitle">TRAINING COURSE</p>
+            </div>
+          </div>
+          <div className="pricing__right">
+            {}
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
