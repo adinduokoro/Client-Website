c3793f5163f98fd7f513c985c20e482b34b68364
Addie Okoro <adinduokoro95@gmail.com>
2023-06-06 21:14:54 -0400

    Classes
    Update

--- src/components/Classes/Classes.js
+++ src/components/Classes/Classes.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Classes.css";
 import logo from "../../assets/images/washington-logo.png";
-import { products } from "./data.js"
+import { products } from "./data.js";
 
 const Classes = () => {
   return (
-    <section className="classes" id="classes" style={{ scrollMarginTop: "7.5rem" }}>
+    <section
+      className="classes"
+      id="classes"
+      style={{ scrollMarginTop: "7.5rem" }}
+    >
       <div className="classes__container container">
         <h2 className="section__title">Classes & Training</h2>
         <span className="section__subtitle">Course Pricing</span>
@@ -19,18 +23,23 @@ const Classes = () => {
           </div>
           <div className="pricing__right">
             {products.map((product, index) => {
-              return(
+              return (
                 <div className="product__card" key={index}>
                   <p className="product__title">{product.title}</p>
                   <div className="price__container">
-                    <hr className="price__line"/>
+                    <hr className="price__line" />
                     <p className="product__price">{product.price}</p>
                   </div>
                 </div>
-              )
+              );
             })}
           </div>
         </div>
+        <p className="disclaimer">
+          Fingerprints are required to be done prior to class <br />
+          MD state police has $75.00 (wear and carry) $50.00 (HQL) application fee <br />
+          ***PRICES MAY VARY***
+        </p>
       </div>
     </section>
   );
