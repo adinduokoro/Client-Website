2635feac8ab5399e39925f52d49ba7bbbe9ee5d6
Addie Okoro <adinduokoro95@gmail.com>
2023-06-12 15:12:25 -0400

    Gallery
    Update

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -13,37 +13,62 @@ import { v4 } from "uuid";
 const Gallery = () => {
   const [imageUpload, setImageUpload] = useState(null);
   const [imageList, setImageList] = useState([]);
+  const [visible, setVisible] = useState(8)
   const storage = getStorage();
-  
-  const uploadImage = () => {
-    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-    if (imageUpload !== null);
-    uploadBytes(imageRef, imageUpload).then((response) => {
-      getDownloadURL(response.ref).then((url) => {
-        setImageList(prev => [...prev, url])
-      })
-    });
-  };
-  
-  useEffect(() => {
-    const imageListRef = ref(storage, "images/")
-    listAll(imageListRef).then(response => {
-      response.items.forEach(item => {
-        getDownloadURL(item).then((url) => {
-          setImageList(prev => [...prev, url])
-        })
-      })
-    })
-  }, [])
 
+  useEffect(() => {
+    const imageListRef = ref(storage, "images/");
+    listAll(imageListRef)
+      .then((response) => {
+        response.items.forEach((item) => {
+          getDownloadURL(item)
+            .then((url) => {
+              setImageList((prev) => [...prev, url]);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const uploadImage = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload)
+      .then((response) => {
+        getDownloadURL(response.ref)
+          .then((url) => {
+            setImageList((prev) => [...prev, url]);
+            setImageUpload(null)
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const handleDelete = (url) => {
-    const newList = imageList.filter(f => f !== url)
-    setImageList(newList)
-    const deleteRef = ref(storage, url)
-    deleteObject(deleteRef).then(() => {
-      console.log("item deleted")
-    })
+    const newList = imageList.filter((f) => f !== url);
+    setImageList(newList);
+    const deleteRef = ref(storage, url);
+    deleteObject(deleteRef)
+      .then(() => {
+        console.log("item deleted");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleLoadMore = () => {
+    setVisible(visible + 4)
   }
 
   return (
@@ -51,16 +76,21 @@ const Gallery = () => {
       <div className="gallery__container container">
         <h2 className="section__title">Gallery</h2>
         <span className="section__subtitle">Follow Us on Instagram</span>
-        {imageList.map((url, index) => {
-          return(
-            <img src={url} alt="" key={index} onClick={() => handleDelete(url)}/>
-          )
-        })}
-        <input
+        <ul className="image__container">
+          {imageList.slice(0, visible).map((url, index) => {
+            return (
+              <li className="imageCard" key={index} onClick={() => ("")}>
+                <img src={url} alt="" />
+              </li>
+            );
+          })}
+        </ul>
+        {visible >=  imageList.length ? ""  : <button className="load__more" onClick={handleLoadMore} disabled={visible === 24}>load more...</button> }
+        {/* <input
           type="file"
           onChange={(event) => setImageUpload(event.target.files[0])}
         />
-        <button onClick={uploadImage}>upload</button>
+        <button onClick={uploadImage}>upload</button> */}
       </div>
     </section>
   );
