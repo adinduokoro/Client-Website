708fa166d102eabd035bc1430bd0db02ef4e2fec
Addie Okoro <adinduokoro95@gmail.com>
2023-06-12 13:45:46 -0400

    Gallery
    Working on gallery functionality for admin.

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,40 +1,66 @@
 import React, { useEffect, useState } from "react";
 import "./Gallery.css";
+import {
+  getStorage,
+  ref,
+  listAll,
+  getDownloadURL,
+  deleteObject,
+  uploadBytes,
+} from "firebase/storage";
+import { v4 } from "uuid";
 
 const Gallery = () => {
-  const [feed, setFeed] = useState([]);
-  const [visible, setVisible] = useState(12)
-
-  const handleClick = () => {
-    setVisible(visible + 6)
-  }
-
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageList, setImageList] = useState([]);
+  const storage = getStorage();
+  
+  const uploadImage = () => {
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    if (imageUpload !== null);
+    uploadBytes(imageRef, imageUpload).then((response) => {
+      getDownloadURL(response.ref).then((url) => {
+        setImageList(prev => [...prev, url])
+      })
+    });
+  };
+  
   useEffect(() => {
-    const URL = `https://graph.instagram.com/me/media?fields=id,media_url,permalink,caption&access_token=${process.env.REACT_APP_INSTAGRAM_TOKEN}`;
+    const imageListRef = ref(storage, "images/")
+    listAll(imageListRef).then(response => {
+      response.items.forEach(item => {
+        getDownloadURL(item).then((url) => {
+          setImageList(prev => [...prev, url])
+        })
+      })
+    })
+  }, [])
 
-    fetch(URL)
-      .then((response) => response.json())
-      .then((data) => setFeed(data.data));
-  }, []);
+
+  const handleDelete = (url) => {
+    const newList = imageList.filter(f => f !== url)
+    setImageList(newList)
+    const deleteRef = ref(storage, url)
+    deleteObject(deleteRef).then(() => {
+      console.log("item deleted")
+    })
+  }
 
   return (
     <section className="gallery">
       <div className="gallery__container container">
         <h2 className="section__title">Gallery</h2>
         <span className="section__subtitle">Follow Us on Instagram</span>
-        <ul className="instagramFeed__container">
-          {feed.slice(0, visible).map((post) => {
-            return (
-              <li className="postCard" key={post.id}>
-                <a href={post.permalink} target="_blank">
-                  <img src={post.media_url} alt={post.caption} />
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-        {visible === 24 ? ""  : <button className="load__more" onClick={handleClick} disabled={visible === 24}>load more...</button> }
-        
+        {imageList.map((url, index) => {
+          return(
+            <img src={url} alt="" key={index} onClick={() => handleDelete(url)}/>
+          )
+        })}
+        <input
+          type="file"
+          onChange={(event) => setImageUpload(event.target.files[0])}
+        />
+        <button onClick={uploadImage}>upload</button>
       </div>
     </section>
   );
